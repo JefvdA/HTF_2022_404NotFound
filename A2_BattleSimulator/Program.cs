a804a6178213d6fad7a6fb2418412bde61265623
JefvdA <jef.v.d.a@live.be>
2022-11-15 14:36:17 +0100

    Convert ApiResult to Battle

--- A2_BattleSimulator/Program.cs
+++ A2_BattleSimulator/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("THIS IS A2!");
+﻿using System.Net.Http.Json;
+using A2_BattleSimulator;
+using DataLayer;
+using DataLayer.Enums;
+
+const string token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiMiIsIm5iZiI6MTY2ODUwMzE3NywiZXhwIjoxNjY4NTg5NTc3LCJpYXQiOjE2Njg1MDMxNzd9.k2hY9b8QAfoPkqwhut0O3wIR1TZPNM89ryPW53n6lHq68pdnNBSLBLWu6i-IT5Wjs9NAbsq7L5tWXlvnS663LA";
+
+ApiClient.BearerToken = token;
+
+var result = await ApiClient.GetSample(EChallengeTrack.A, EChallengeDifficulty.Medium);
+var battle = await result.Content!.ReadFromJsonAsync<Battle>();
+
