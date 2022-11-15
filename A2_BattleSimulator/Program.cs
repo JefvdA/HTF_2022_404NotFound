80541ffb902cd13303d125cf0341d2c319c51838
JefvdA <jef.v.d.a@live.be>
2022-11-15 14:56:35 +0100

    Feat add battle simulation

--- A2_BattleSimulator/Program.cs
+++ A2_BattleSimulator/Program.cs
@@ -7,6 +7,39 @@ const string token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiMiIsIm5iZ
 
 ApiClient.BearerToken = token;
 
-var result = await ApiClient.GetSample(EChallengeTrack.A, EChallengeDifficulty.Medium);
+var result = await ApiClient.GetPuzzle(EChallengeTrack.A, EChallengeDifficulty.Medium);
 var battle = await result.Content!.ReadFromJsonAsync<Battle>();
 
+while(battle!.teamA.Count > 0 && battle!.teamB.Count > 0)
+{
+    var player1 = battle!.teamA[0];
+    var player2 = battle!.teamB[0];
+
+    var currentPlayer = player1;
+    if (player2.speed > player1.speed)
+        currentPlayer = player2;
+    
+    while (player1.health > 0 && player2.health > 0)
+    {
+        var otherPlayer = player2;
+        if (currentPlayer == player2)
+            otherPlayer = player1;
+
+        otherPlayer.health -= currentPlayer.strength;
+        
+        if (otherPlayer.health <= 0)
+            break;
+        
+        currentPlayer = otherPlayer;
+    }
+    
+    if (player1.health <= 0)
+        battle!.teamA.Remove(player1);
+    if (player2.health <= 0)
+        battle!.teamB.Remove(player2);
+}
+
+var winner = battle!.teamA.Count > 0 ? "TeamA" : "TeamB";
+Console.WriteLine($"The winner is: {winner}");
+
+await ApiClient.PostPuzzle(EChallengeTrack.A, EChallengeDifficulty.Medium, winner);
